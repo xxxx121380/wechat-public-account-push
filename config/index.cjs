8ece5d3ac1ca1bce9a2d78ad9919550be6d1a500
xxxx121380 <67416330+xxxx121380@users.noreply.github.com>
2024-01-25 15:53:02 -0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -26,18 +26,18 @@ const USER_CONFIG = {
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '10-27',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '宝贝', year: '1998', date: '10-27',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '老公', year: '1996', date: '05-15',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '纪念日', year: '2019', date: '04-10',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
